3f1e0bf63ed066621f5b5cd9dc64de980d465b6b
Babatunde Koiki <koikibabatunde14@gmail.com>
2022-08-07 00:49:02 +0100

    feat(search): recursive binary search

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,6 +1,7 @@
 class BinarySearch:
     def __init__(self, data) -> None:
         self.data = data
+        self.index = 0
     
     def find(self, target):
         first = 0
@@ -16,5 +17,31 @@ class BinarySearch:
                 last = mid_idx - 1
         return -1
 
+class RecursiveBinarySearch:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.data_copy = data
+        self.index = 0
+    
+    def find(self, target):
+        if len(self.data_copy) == 0:
+            return -1
+        
+        mid_idx = len(self.data_copy) // 2
+        self.index += mid_idx
+        mid_value = self.data_copy[mid_idx]
+        if target == mid_value:
+            return self.index
+        else:
+            if target > mid_value:
+                self.data_copy = self.data_copy[mid_idx:]
+                return self.find(target)
+            else:
+                self.data_copy = self.data_copy[:mid_idx]
+                return self.find(target)
+
 a = BinarySearch([1, 2, 4, 5, 6, 7, 8, 8, 9, 10])
 print(a.find(10))
+
+b = RecursiveBinarySearch([1, 2, 4, 5, 6, 7, 8, 8, 9, 10])
+print(b.find(10))
